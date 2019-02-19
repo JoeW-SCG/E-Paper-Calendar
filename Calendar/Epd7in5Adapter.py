7596b7703ddd27a0416c51089f5c8b62337522f8
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-19 21:43:53 +0100

    Implemented EpdDisplayAdapters

---
+++ Calendar/Epd7in5Adapter.py
@@ -0,0 +1,47 @@
+from EpdAdapter import EpdAdapter, DISPLAY_REFRESH, DATA_START_TRANSMISSION_1
+
+class Epd7in5Adapter (EpdAdapter):
+	def __init__ ():
+		super(Epd7in5Adapter, self).__init__(640, 384)
+
+    def display_frame (self, frame_buffer):
+        self.send_command(DATA_START_TRANSMISSION_1)
+        for i in range(0, 30720):
+            temp1 = frame_buffer[i]
+            j = 0
+            while (j < 8):
+                if(temp1 & 0x80):
+                    temp2 = 0x03
+                else:
+                    temp2 = 0x00
+                temp2 = (temp2 << 4) & 0xFF
+                temp1 = (temp1 << 1) & 0xFF
+                j += 1
+                if(temp1 & 0x80):
+                    temp2 |= 0x03
+                else:
+                    temp2 |= 0x00
+                temp1 = (temp1 << 1) & 0xFF
+                self.send_data(temp2)
+                j += 1
+        self.send_command(DISPLAY_REFRESH)
+        self.delay_ms(100)
+        self.wait_until_idle()
+
+    def get_frame_buffer (self, image):
+        buf = [0x00] * int(self.width * self.height / 8)
+        # Set buffer to value of Python Imaging Library image.
+        # Image must be in mode 1.
+        image_monocolor = image.convert('1') #with ot withour dithering?
+        imwidth, imheight = image_monocolor.size
+        if imwidth != self.width or imheight != self.height:
+            raise ValueError('Image must be same dimensions as display \
+                ({0}x{1}).' .format(self.width, self.height))
+
+        pixels = image_monocolor.load()
+        for y in range(self.height):
+            for x in range(self.width):
+                # Set the bits for the column of pixels at the current position.
+                if pixels[x, y] != 0:
+                    buf[int((x + y * self.width) / 8)] |= 0x80 >> (x % 8)
+        return buf
---
+++ Calendar/Epd7in5bAdapter.py
@@ -0,0 +1,60 @@
+from EpdAdapter import EpdAdapter, DISPLAY_REFRESH, DATA_START_TRANSMISSION_1
+
+class Epd7in5bAdapter (EpdAdapter):
+	def __init__ ():
+		super(Epd7in5bAdapter, self).__init__(640, 384)
+
+    def display_frame (self, frame_buffer):
+        self.send_command(DATA_START_TRANSMISSION_1)
+        for i in range(0, int(self.width / 4 * self.height)):
+            #the above line had to be modified due to python2 -> python3
+            #the issue lies in division, which returns integers in python2
+            #but floats in python3
+            temp1 = frame_buffer[i]
+            j = 0
+            while (j < 4):
+                if ((temp1 & 0xC0) == 0xC0):
+                    temp2 = 0x03
+                elif ((temp1 & 0xC0) == 0x00):
+                    temp2 = 0x00
+                else:
+                    temp2 = 0x04
+                temp2 = (temp2 << 4) & 0xFF
+                temp1 = (temp1 << 2) & 0xFF
+                j += 1
+                if((temp1 & 0xC0) == 0xC0):
+                    temp2 |= 0x03
+                elif ((temp1 & 0xC0) == 0x00):
+                    temp2 |= 0x00
+                else:
+                    temp2 |= 0x04
+                temp1 = (temp1 << 2) & 0xFF
+                self.send_data(temp2)
+                j += 1
+        self.send_command(DISPLAY_REFRESH)
+        self.delay_ms(100)
+        self.wait_until_idle()
+
+    def get_frame_buffer (self, image):
+        buf = [0x00] * int(self.width * self.height / 4)
+        # Set buffer to value of Python Imaging Library image.
+        # Image must be in mode L.
+        image_grayscale = image.convert('L', dither=None)
+        imwidth, imheight = image_grayscale.size
+        if imwidth != self.width or imheight != self.height:
+            raise ValueError('Image must be same dimensions as display \
+                ({0}x{1}).' .format(self.width, self.height))
+
+        pixels = image_grayscale.load()
+        for y in range(self.height):
+            for x in range(self.width):
+                # Set the bits for the column of pixels at the current position.
+                if pixels[x, y] < 75: #was 64          # black
+                    buf[int((x + y * self.width) / 4)] &= ~(0xC0 >> (x % 4 * 2))
+                elif pixels[x, y] < 110:  #was 192   # convert gray to red
+                    buf[int((x + y * self.width) / 4)] &= ~(0xC0 >> (x % 4 * 2))
+                    buf[int((x + y * self.width) / 4)] |= 0x40 >> (x % 4 * 2)
+                else:                           # white
+                    buf[int((x + y * self.width) / 4)] |= 0xC0 >> (x % 4 * 2)
+        return buf #due to python2 -> python3, int had to be added in 'get_frame
+                   #_buffer 
