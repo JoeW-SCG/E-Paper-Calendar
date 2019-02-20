91a160512f6baf1711a4c3df4164619925556431
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-20 21:17:39 +0100

    Removed superfluos status print.

--- Calendar/E-Paper.py
+++ Calendar/E-Paper.py
@@ -275,7 +275,6 @@ def main():
             except IndexError:
                 pass
 
-            print('Initialising E-Paper Display')
             epd.render(image)
 
             for i in range(1):
