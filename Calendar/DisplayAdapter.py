679fed425d61944183da66cbca5e77a7e43611c1
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-20 20:00:08 +0100

    Bounded width and height of DisplayAdapter to constructor.

--- Calendar/DisplayAdapter.py
+++ Calendar/DisplayAdapter.py
@@ -1,8 +1,11 @@
 class DisplayAdapter (object):
-	"""Interface for CalendarDesign output channels.
-    Needs implementation of width and height properties."""
-	def render (self, design):
-		raise NotImplementedError("Functions needs to be implemented")
+    """Interface for CalendarDesign output channels."""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-	def calibrate (self):
-		raise NotImplementedError("Functions needs to be implemented")
+    def render (self, design):
+        raise NotImplementedError("Functions needs to be implemented")
+
+    def calibrate (self):
+        raise NotImplementedError("Functions needs to be implemented")
