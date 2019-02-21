2b4406d613bb132dc05ae9f7481c5ea8565931b2
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-21 17:17:18 +0100

    Defined abstract weather forecast source interface.

---
+++ Calendar/WeatherInterface.py
@@ -0,0 +1,12 @@
+from WeatherForecast import WeatherForecast
+
+class WeatherInterface (object):
+    """Interface for fetching and processing weather forecast information."""
+    def is_available (self):
+        raise NotImplementedError("Functions needs to be implemented")
+
+    def get_forecast_in_days (self, offset_by_days):
+        raise NotImplementedError("Functions needs to be implemented")
+
+    def get_today_forecast (self):
+        raise NotImplementedError("Functions needs to be implemented")
