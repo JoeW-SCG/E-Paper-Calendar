5d88baca088a990b7b90eced8844f826c56925fd
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-23 19:26:58 +0100

    Added properties

--- Calendar/WeatherForecast.py
+++ Calendar/WeatherForecast.py
@@ -2,9 +2,11 @@ class WeatherForecast (object):
     """Defines a weather forecast, independent of any implementation"""
     def __init__ (self):
         self.air_temperature = None
+        self.air_pressure = None
         self.air_humidity = None
         self.rain_probability = None
         self.rain_amount = None
+        self.snow_amount = None
         self.sunrise = None
         self.sunset = None
         self.moon_phase = None
@@ -13,8 +15,10 @@ class WeatherForecast (object):
 
         self.icon = None
         self.short_description = None
+        self.detailed_description = None
 
         self.units = None
         self.date = None
         self.place = None
+        self.forecast_datetime = None
         self.fetch_time = None
