1a330d9e73caae78748903d906b2245b3ffd96e4
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-25 21:43:27 +0100

    Changed names

--- Calendar/CalendarEvent.py
+++ Calendar/CalendarEvent.py
@@ -1,8 +1,7 @@
 class CalendarEvent (object):
     """Defines a calendar event, independent of any implementation"""
     def __init__ (self):
-        self.date = None
-        self.time = None
+        self.datetime = None
         self.duration = None
 
         self.title = None
@@ -10,5 +9,7 @@ class CalendarEvent (object):
         self.attendees = []
         self.highlight = None
 
-        self.place = None
-        self.fetch_time = None
+        self.calendar_name = None
+
+        self.location = None
+        self.fetch_datetime = None
