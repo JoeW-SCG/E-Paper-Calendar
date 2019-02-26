7f9a6a3102b9c65c9da23bb7171b9d13f4900642
Maximilian Giller <m.giller.dev@gmail.com>
2019-02-26 17:30:53 +0100

    Added and removed some parameters in CalendarInterface

--- Calendar/CalendarInterface.py
+++ Calendar/CalendarInterface.py
@@ -2,17 +2,17 @@ from DataSourceInterface import DataSourceInterface
 
 class CalendarInterface (DataSourceInterface):
     """Interface for fetching and processing calendar event information."""
-    def get_upcoming_events(self, count):
+    def get_upcoming_events(self):
         raise NotImplementedError("Functions needs to be implemented")
 
     def get_today_events(self):
         raise NotImplementedError("Functions needs to be implemented")
 
-    def get_day_events(self):
+    def get_day_events(self, date):
         raise NotImplementedError("Functions needs to be implemented")
 
-    def get_month_events(self, count):
+    def get_month_events(self, month):
         raise NotImplementedError("Functions needs to be implemented")
 
-    def get_week_events(self, count):
+    def get_week_events(self, week):
         raise NotImplementedError("Functions needs to be implemented")
