b746c13113dd58c36c90254e5c1c4dcd386771c1
Maximilian Giller <m.giller.dev@gmail.com>
2019-03-03 18:13:51 +0100

    Implemented alignment and removed leading zero in date

--- Calendar/EventListDesign.py
+++ Calendar/EventListDesign.py
@@ -16,12 +16,20 @@ class EventListDesign (DesignEntity):
         self.__fill_event_matrix__()
         
         max_col_size = [int(col_sizes[0] * self.size[0]), int(col_sizes[1] * self.size[0])]
+        col_hori_alignment = ['right', 'left']
 
-        table_design = TableTextDesign(self.size, line_spacing=3, col_spacing=5, text_matrix=self.__event_matrix__, fontsize = self.text_size, mask=False, max_col_size = max_col_size, truncate_cols=False)
+        table_design = TableTextDesign(self.size, line_spacing=3, col_spacing=10, text_matrix=self.__event_matrix__, fontsize = self.text_size, column_horizontal_alignments=col_hori_alignment, mask=False, max_col_size = max_col_size, truncate_cols=False)
         self.draw_design(table_design)
     
     def __get_formatted_event__ (self, event):
-        return [ event.begin_datetime.strftime('%d %b'), event.title ]
+        date = event.begin_datetime.strftime('%d %b')
+        date = self.__remove_leading_zero__(date)
+        return [ date, event.title ]
+
+    def __remove_leading_zero__(self, text):
+        while text[0] is '0':
+            text = text[1:]
+        return text
     
     def __fill_event_matrix__ (self):
         for event in self.calendar.get_upcoming_events():
