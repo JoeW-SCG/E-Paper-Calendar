6f8762d6ffe7ed95aef0c9a857fc6c480d3cd4be
Maximilian Giller <m.giller.dev@gmail.com>
2019-03-03 14:00:10 +0100

    Added EventListDesign

---
+++ Calendar/EventListDesign.py
@@ -0,0 +1,29 @@
+from DesignEntity import DesignEntity
+from TableTextDesign import TableTextDesign
+
+col_sizes = [0.15, 0.85]
+
+class EventListDesign (DesignEntity):
+    """Creates a TableTextDesign filled with event
+    begin date and title"""
+    def __init__ (self, size, calendar, event_count = None, text_size = 16):
+        super(EventListDesign, self).__init__(size)
+        self.calendar = calendar
+        self.__event_matrix__ = []
+        self.text_size = text_size
+
+    def __finish_image__ (self):
+        self.__fill_event_matrix__()
+        
+        max_col_size = [int(col_sizes[0] * self.size[0]), int(col_sizes[1] * self.size[0])]
+
+        table_design = TableTextDesign(self.size, text_matrix=self.__event_matrix__, fontsize = self.text_size, mask=False, max_col_size = max_col_size)
+        self.draw_design(table_design)
+    
+    def __get_formatted_event__ (self, event):
+        return [ event.begin_datetime.strftime('%d %b'), event.title ]
+    
+    def __fill_event_matrix__ (self):
+        for event in self.calendar.get_upcoming_events():
+            row = self.__get_formatted_event__(event)
+            self.__event_matrix__.append(row)
