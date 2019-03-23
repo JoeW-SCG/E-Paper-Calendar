544c23954df3cf3a6e335663e0fa0824705b1c80
Max G <m.giller.dev@gmail.com>
2019-03-23 12:24:43 +0100

    Added rrule property

--- Calendar/CalendarEvent.py
+++ Calendar/CalendarEvent.py
@@ -5,6 +5,7 @@ class CalendarEvent (object):
         self.end_datetime = None
         self.duration = None
         self.allday = None
+        self.rrule = None
 
         self.title = None
         self.description = None
