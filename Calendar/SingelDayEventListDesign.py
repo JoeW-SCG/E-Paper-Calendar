99bba341a9eee6d166a4062580df728b14cfe9ff
Max G <m.giller.dev@gmail.com>
2019-03-24 21:05:12 +0100

    Added missing color parameter tranmission

--- Calendar/SingelDayEventListDesign.py
+++ Calendar/SingelDayEventListDesign.py
@@ -9,7 +9,7 @@ class SingelDayEventListDesign (EventListDesign):
     """Specialized event list for day list design."""
     def __init__ (self, size, events, font_size = 16, line_spacing=2, col_spacing=5, general_color="black", background_color="white", highlight_color="red"):
         prefix_func = lambda x : self.__get_event_prefix__(x)
-        super().__init__(size, events, text_size=font_size, line_spacing=line_spacing, col_spacing=col_spacing, event_prefix_func=prefix_func, font_family=font, show_more_info=True)
+        super().__init__(size, events, text_size=font_size, line_spacing=line_spacing, col_spacing=col_spacing, event_prefix_func=prefix_func, font_family=font, show_more_info=True, general_color=general_color, background_color=background_color, highlight_color = highlight_color)
     
     def __get_event_prefix__ (self, event):
         if event.allday:
